387f5b128a73f8eb63fdea41c46422956a044c90
Roni Bam <roni.bam51@gmail.com>
2023-02-26 20:42:30 +0200

    validations in createVector

--- src/main/java/jobs/CreateVectors.java
+++ src/main/java/jobs/CreateVectors.java
@@ -18,54 +18,49 @@ public class CreateVectors {
     //so we will get - <noun pair>,<path>
     public static class CorpusMapperClass extends Mapper<LongWritable, Text, NounPair, DependencyPath> {
 
-        protected void setup(Mapper context) throws IOException { }
-
         @Override
         public void map(LongWritable lineId, Text line, Mapper.Context context) throws IOException, InterruptedException {
 
             String[] splitLine = line.toString().split("\\t");
-            String path = splitLine[0];
-            String numOfOccurrencesPerPath = splitLine[1];
-            String pathId = splitLine[2];
-            for (int i=3;i<splitLine.length;i++){
-                String [] nouns = splitLine[i].split(",");
-                if (nouns.length  == 2 )
-                    //not final - need fix according to changes in DependencyPath class
-                    context.write(new NounPair(nouns[0],nouns[1]),new DependencyPath(new LongWritable(Long.parseLong(pathId)),new Text(path),new LongWritable(Long.parseLong(numOfOccurrencesPerPath))));
+            if (splitLine.length == 3) {
+                String path = splitLine[0];
+                String numOfOccurrencesPerPath = splitLine[1];
+                String pathId = splitLine[2];
+                for (int i = 3; i < splitLine.length; i++) {
+                    String[] nouns = splitLine[i].split(",");
+                    if (nouns.length == 2)
+                        //not final - need fix according to changes in DependencyPath class
+                        try {
+                            context.write(new NounPair(nouns[0], nouns[1]), new DependencyPath(new LongWritable(Long.parseLong(pathId)), new Text(path), new LongWritable(Long.parseLong(numOfOccurrencesPerPath))));
+                        } catch (NumberFormatException ex) {
+                            System.out.println("failed to parse to long: " + ex.getMessage());
+
+                        }
+                }
             }
         }
     }
 
-    /**the mapper will send the reduce the pair and the boolean classification **/
+    /**
+     * the mapper will send the reduce the pair and the boolean classification
+     **/
     public static class HypernymMapperClass extends Mapper<LongWritable, Text, NounPair, DependencyPath> {
-
-        protected void setup(Mapper context) throws IOException { }
 
         @Override
         public void map(LongWritable lineId, Text line, Mapper.Context context) throws IOException, InterruptedException {
 
             String[] splitLine = line.toString().split("\\t");
-            if (splitLine.length == 3){
+            if (splitLine.length == 3) {
                 String noun1 = splitLine[0];
                 String noun2 = splitLine[1];
                 String isHypernym = splitLine[2];
 
-                context.write(new NounPair(noun1,noun2,(isHypernym.equals("True"))),new DependencyPath());
+                context.write(new NounPair(noun1, noun2, (isHypernym.equals("True"))), new DependencyPath());
             }
         }
     }
 
-
-  /*  public static class CombinerClass extends Reducer<NounPair, DependencyPath,NounPair, DependencyPath> {
-
-        //should sum up together all the occurrences of same noun pair;
-        @Override
-        public void reduce(NounPair nounPair, Iterable<DependencyPath> paths, Context context) throws IOException, InterruptedException {
-            //think about it...
-        }
-    } */
-
-    public static class PartitionerClass extends Partitioner<NounPair,DependencyPath> {
+    public static class PartitionerClass extends Partitioner<NounPair, DependencyPath> {
 
         @Override
         public int getPartition(NounPair key, DependencyPath value, int numPartitions) {
@@ -91,31 +86,32 @@ public class CreateVectors {
 
             boolean isFromHypernymFile = false;
 
-            for (DependencyPath currPath : paths){
-                    int vectorId = (int) currPath.idInVector.get();
-                    if (vectorId != 0) {
-                        featureVector.setSize((int) featureLexiconSize);
-                        try {
-                            Integer curr = featureVector.get(vectorId);
-                            if (curr != null) {
-                                featureVector.set(vectorId -1 , curr + (int) currPath.numOfOccurrences.get());
+                for (DependencyPath currPath : paths) {
+                    try {
+                        int vectorId = (int) currPath.idInVector.get();
+                        if (vectorId != 0) {
+                            featureVector.setSize((int) featureLexiconSize);
+                            try {
+                                Integer curr = featureVector.get(vectorId);
+                                if (curr != null) {
+                                    featureVector.set(vectorId - 1, curr + (int) currPath.numOfOccurrences.get());
+                                } else {
+                                    featureVector.set(vectorId - 1, (int) currPath.numOfOccurrences.get());
+                                }
+                            } catch (Exception ex) {
+                                featureVector.set(vectorId - 1, (int) currPath.numOfOccurrences.get());
                             }
-                            else{
-                                featureVector.set(vectorId -1 ,(int) currPath.numOfOccurrences.get());
-                            }
-                        } catch (Exception ex){
-                            featureVector.set(vectorId -1 ,(int) currPath.numOfOccurrences.get());
+                        } else {
+                            isFromHypernymFile = true;
                         }
+                    } catch (ClassCastException ex) {
+                        System.out.println("failed to cast to int: " + ex.getMessage());
                     }
-                    else{
-                        isFromHypernymFile = true;
-                    }
-
                 }
                 if (featureVector.size() > 0 && isFromHypernymFile) {
-                    for (int i = 0; i < featureVector.size() ; i ++){
-                        if (featureVector.get(i) == null){
-                            featureVector.set(i,0);
+                    for (int i = 0; i < featureVector.size(); i++) {
+                        if (featureVector.get(i) == null) {
+                            featureVector.set(i, 0);
                         }
                     }
                     context.write(new Text(featureVector.toString()), nounPair.isHypernym);
