0037224ecdd01e68ecead863855bf85ab92ec1d8
Roni Bam <roni.bam51@gmail.com>
2023-02-28 11:33:24 +0200

    classification algorithms options

--- src/main/java/localClasses/ClassifierTrainer.java
+++ src/main/java/localClasses/ClassifierTrainer.java
@@ -3,7 +3,10 @@ package localClasses;
 import weka.classifiers.Classifier;
 import weka.classifiers.bayes.NaiveBayes;
 import weka.classifiers.bayes.NaiveBayesUpdateable;
+import weka.classifiers.lazy.IBk;
+import weka.classifiers.trees.J48;
 import weka.classifiers.trees.M5P;
+import weka.classifiers.trees.RandomTree;
 import weka.core.Instances;
 
 /** this class recieves as input a converted data in the format of Json File. the data structure would be Vector +  Boolean
@@ -22,9 +25,28 @@ public class ClassifierTrainer {
 
     public static Classifier buildClassifier(Instances data, boolean isIncremental) throws Exception {
 
+        System.out.println("building classifier");
+
+       // naive bayes
         NaiveBayes classifier = new NaiveBayes();
         classifier.buildClassifier(data);
         return classifier;
+
+        //j48
+        /*J48 j48 = new J48();
+        j48.buildClassifier(data);
+        return j48; */
+
+        //lbk
+        /*IBk ibk = new IBk();
+        ibk.buildClassifier(data);
+        return ibk; */
+
+       //random tree
+        /*RandomTree randomTree = new RandomTree();
+        randomTree.buildClassifier(data);
+        return randomTree; */
+
     }
 
 }
